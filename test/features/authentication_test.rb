c7119058420729b73498ef8f87cebe22d2d169e2
lbuell <liamcbuell@gmail.com>
2014-02-03 15:48:12 -0800

    Added auth tests, HTML template

--- test/features/authentication_test.rb
+++ test/features/authentication_test.rb
@@ -26,10 +26,55 @@ feature "Home Screen" do
       fill_in "Email", with: users(:liam).email
       fill_in "Password", with: "password"
 
-      click_button "Sign in"
+      click_button "Log in"
 
       #save_and_open_page
-      page.must_have_content "Welcome! You have signed in successfully."
+      page.must_have_content "Signed in successfully."
       page.wont_have_content "There was a problem with your sign up"
+    end
+  end
+  feature "Sign out Path" do
+    scenario "User can Sign out" do
+      visit new_user_session_path
+      fill_in "Email", with: users(:liam).email
+      fill_in "Password", with: "password"
+
+      click_button "Log in"
+      click_on "Logout"
+      page.must_have_content "Signed out successfully."
+    end
+  end
+  feature "User account deletion" do
+    scenario "A user can delete account" do
+      visit new_user_session_path
+      fill_in "Email", with: users(:liam).email
+      fill_in "Password", with: "password"
+
+      click_button "Log in"
+      #save_and_open_page
+      click_on "Edit account"
+      click_on "Cancel my account"
+
+      page.must_have_content "Bye! Your account was successfully cancelled. We hope to see you again soon."
+    end
+  end
+  feature "User account password update" do
+    scenario "A user can edit account and change password" do
+      visit new_user_session_path
+      fill_in "Email", with: users(:liam).email
+      fill_in "Password", with: "password"
+
+      click_button "Log in"
+      #save_and_open_page
+      click_on "Edit account"
+      fill_in "Password", with: "password"
+      fill_in "Password confirmation", with: "password"
+      fill_in "Current password", with: "password"
+      save_and_open_page
+      click_on "Update"
+
+      page.must_have_content "You updated your account successfully."
+    end
+  end
 end
 
