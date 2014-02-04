2ec7f84ed6a97cc0bfadf72431ca0d65330a20ea
lbuell <liamcbuell@gmail.com>
2014-02-03 20:15:05 -0800

    Added Omniauth-twitter

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Myfamilysinfo::Application.routes.draw do
   root :to => "home#index"
-  devise_for :users, :controllers => {:registrations => "registrations"}
+ devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
   resources :users
 end
