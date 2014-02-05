533d0f0b5c53a9cdc6cd385f9631d0f8be2ef3f2
lbuell <liamcbuell@gmail.com>
2014-02-04 19:34:12 -0800

    Adding Kids

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -1,20 +1,20 @@
+
 class KidsController < ApplicationController
   def new
     @user = current_user
     @kid = Kid.new
-    #@kid.relationships.build(user: current_user)
   end
 
   def create
      @kid = Kid.create(kid_params)
      @relationship = Relationship.create(kid_id: @kid.id, user_id: current_user.id)
-     #redirect_to current_user_path
-  end
+   end
 
 
 
    private
-    def kid_params
-      params.require(:kid).permit(:name, relationships_attributes: [:role, :user_id])
-    end
-end
+     def kid_params
+      params.require(:kid).permit(:name, relationship_attributes: [:role, :user_id])
+
+     end
+ end
