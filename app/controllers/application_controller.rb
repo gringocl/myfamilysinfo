c94e7bacf9b6e9d4890025a275d4ffce8c9ebe31
lbuell <liamcbuell@gmail.com>
2014-02-06 08:26:20 -0800

    Added facebook login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  # def after_sign_in_path_for(resource_or_scope)
-  #   if request.env['omniauth.origin']
-  #      current_user
-  #    end
-  # end
+  private
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
 end
