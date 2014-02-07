7f5539aba98627e5000052fca490be25a08cc449
lbuell <liamcbuell@gmail.com>
2014-02-07 12:50:28 -0800

    Facebook and tests

--- test/features/more_info_static_page_test.rb
+++ test/features/more_info_static_page_test.rb
@@ -4,8 +4,6 @@ feature "More info section of home page" do
   scenario "User can learn more about our app" do
     visit root_path
     page.must_have_link "Use our App!"
-    page.must_have_content "Add a Kid"
-    page.must_have_content "Add a Reminder"
     page.must_have_content "Sign Up Now!"
   end
 end
