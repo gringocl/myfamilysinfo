3b8c9dde5c4762393908145f91aa49bb52562a63
lbuell <liamcbuell@gmail.com>
2014-02-07 09:43:36 -0800

    Tests and Forms update

--- test/features/managing_kids_test.rb
+++ test/features/managing_kids_test.rb
@@ -6,7 +6,7 @@ feature "Add a kid" do
     #save_and_open_page
     click_on "Add a kid"
     fill_in "Name", with: "Tim"
-    click_on "Create Kid"
+    click_on "Add"
     page.must_have_content "Tim"
     #page.must_have_content "Remove Kid"
   end
@@ -15,7 +15,7 @@ feature "Add a kid" do
     sign_in(:mother)
     page.find("a[data-confirm][href='#{kid_path(kids(:Jenny))}']").click
     #first(:link, 'Delete!').click
-    #save_and_open_page
-    page.must_have_content "Kid Removed"
+    save_and_open_page
+    page.must_have_content "Jenny was removed from your account"
   end
 end
