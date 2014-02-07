f13f8a09134ba14f92c9a3da93d628f9f290beca
Miles Starkenburg <milesstarkenburg@gmail.com>
2014-02-07 10:28:30 -0800

    Lots of styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,11 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require foundation
 //= require_tree .
 $(function() {
   $(document).foundation();
 });
-
-$(function(){ $(document).foundation(); });
